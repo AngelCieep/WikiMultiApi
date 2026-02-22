f1a5a780ed5878d9e83802c89318db674dc88901
Franchu <franvives280026@gmail.com>
2026-02-22 15:33:12 +0100

    componentes Angular

---
+++ frontend-angular/src/app/components/universe-detail/universe-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-universe-detail',
+  imports: [],
+  templateUrl: './universe-detail.html',
+  styleUrl: './universe-detail.css',
+})
+export class UniverseDetail {
+
+}
---
+++ frontend-angular/src/app/components/universes/universes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-universes',
+  imports: [],
+  templateUrl: './universes.html',
+  styleUrl: './universes.css',
+})
+export class Universes {
+
+}
