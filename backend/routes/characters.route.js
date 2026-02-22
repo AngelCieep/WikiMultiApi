e9f59a01bc7900477e5abf8f51d3a0e552cc000e
AngelCieep <angelmaribla777@digitechfp.com>
2026-02-22 12:34:06 +0100

    url change

--- backend/routes/characters.route.js
+++ backend/routes/characters.route.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 //Rutas
 router.get('/all', charsCtrl.getCharacters);
-router.get('/:id', charsCtrl.getCharacter);
+router.get('/character/:id', charsCtrl.getCharacter);
 router.post('/', charsCtrl.addCharacter);
 router.post('/bulk', charsCtrl.addCharacters);
-router.post('/Universe/:id', charsCtrl.getCharactersByUniverse);
+router.post('/universe/:id', charsCtrl.getCharactersByUniverse);
 router.put('/:id', charsCtrl.updateCharacter);
 router.delete('/:id', charsCtrl.deleteCharacter);
 
