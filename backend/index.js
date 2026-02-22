085d11cb515fc438278add986f726adf31595f42
Franchu <franvives280026@gmail.com>
2026-02-22 01:14:23 +0100

    primeras configuraciones

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,5 @@ app.use('/', (req, res) => res.send('Api funcina en  api/v1'));
 // settings
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
-    console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
+    console.log('Servidor corriendo en el puerto', app.get('port'));
 });
