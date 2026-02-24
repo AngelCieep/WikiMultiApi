b925b881eb02d5d0f60ecf56d3561d1cbe606e48
AngelCieep <angelmaribla777@digitechfp.com>
2026-02-24 21:44:53 +0100

    pre dark

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   title = 'WikiMultiApi';
+
+  readonly images = [
+    '/img/background/pexels-felix-mittermeier-956981_1_11zon_60.webp',
+    '/img/background/pexels-krisof-1252890_2_11zon_60.webp',
+    '/img/background/pexels-philippedonn-1257860_3_11zon_60.webp',
+    '/img/background/pexels-wendywei-2753432_4_11zon_60.webp',
+  ];
+
+  currentIndex = signal(0);
+  nextIndex = signal(1);
+  transitioning = signal(false);
+
+  private intervalId: ReturnType<typeof setInterval> | null = null;
+
+  ngOnInit(): void {
+    // Cambiar imagen cada 2 minutos
+    this.intervalId = setInterval(() => this.cycleImage(), 120_000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) clearInterval(this.intervalId);
+  }
+
+  private cycleImage(): void {
+    const total = this.images.length;
+    this.nextIndex.set((this.currentIndex() + 1) % total);
+    this.transitioning.set(true);
+    setTimeout(() => {
+      this.currentIndex.set(this.nextIndex());
+      this.transitioning.set(false);
+    }, 1500);
+  }
 }
